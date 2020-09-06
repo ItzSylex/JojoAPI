3675b6e0cc3fba8a01c15f40c113b6162c137753
ItzSylex <brandon162000@gmail.com>
2020-09-06 00:53:27 -0600

    debug and config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+app.config['SONIFY_PRETTYPRINT_REGULAR'] = True
 
 from app.routes import endpoints
 from app.routes import error
