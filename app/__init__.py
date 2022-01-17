3f864fdc519394a6fed54584f0dcf87b282d8275
ItzSylex <brandon162000@gmail.com>
2022-01-16 19:50:57 -0600

    Attemp to fix issue#1

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config["DEBUG"] = True
+CORS(app)
 
 from app.routes import endpoints
 from app.routes import error
--- app/routes/endpoints.py
+++ app/routes/endpoints.py
@@ -1,30 +1,13 @@
 from app import app
 from app.getter import GetData
 from flask import request, jsonify
+from flask_cors import CORS, cross_origin
 
 rou = GetData()
 
-# @app.route('/', defaults={'serie': None})
-# @app.route('/<serie>')
-# def endpoint(serie = None):
-#
-#     valid_serie = [
-#         'DiamondIsUnbreakable',
-#         'GoldenWind',
-#         'StardustCrusaders'
-#     ]
-#
-#     if serie is not None and not request.args:
-#         data = rou.get_data(None, None, None)
-#         return jsonify(data), 200
-#
-#     if serie and request.args:
-#         if serie in valid_serie:
-#
-#             data = rou.get_data(serie, None, None)
-
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def get_all_data():
 
     if request.args:
@@ -38,16 +21,19 @@ def get_all_data():
 
 
 @app.route('/StardustCrusaders', methods=['GET'])
+@cross_origin()
 def get_stardust():
     data = rou.get_data('StardustCrusaders', None, None)
     return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @app.route('/DiamondIsUnbreakable', methods=['GET'])
+@cross_origin()
 def get_diamond():
     data = rou.get_data('DiamondIsUnbreakable', None, None)
     return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @app.route('/GoldenWind', methods=['GET'])
+@cross_origin()
 def get_golden():
     data = rou.get_data('GoldenWind', None, None)
     return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
