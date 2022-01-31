c42c9ddc40e806ff405c75bb7a4880c35d5f85d2
ItzSylex <brandon162000@gmail.com>
2022-01-31 03:17:29 -0600

    testing fetch of image

--- scraper/tests.py
+++ scraper/tests.py
@@ -11,4 +11,20 @@ def check_for_datasource(link, data_source):
     print(response_div.contents[0])
 
 
-check_for_datasource("https://jojowiki.com/Star_Platinum", "title")
+# check_for_datasource("https://jojowiki.com/Star_Platinum", "title")
+
+
+def check_for_image(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    response_div = soup.find_all('div', attrs={'title': "Anime", "class": "tabbertab"})
+
+    for each in response_div:
+        new = each.find('a', attrs={'class': "image"})
+
+        for x in new.children:
+            print(x["src"])
+
+
+check_for_image("https://jojowiki.com/Star_Platinum")
